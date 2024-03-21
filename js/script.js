0c6ce151618c5d88383499dfc186ebb06bb6c04d
Mairbek <madiev.1995@list.ru>
2024-03-21 01:56:06 +0100

    Rewriting the functionality on Promise using fetch

--- js/script.js
+++ js/script.js
@@ -213,7 +213,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Forms
 
-
     const forms = document.querySelectorAll('form');
 
     const message = {
@@ -238,33 +237,30 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage)
 
-
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
-
 
             const object = {};
             formData.forEach(function (value, key) {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+            fetch('server.php', {
+                method: "POST",
+                /*                 headers: {
+                                    'Content-type': 'application/json; charset=utf-8'
+                                }, */
+                body: JSON.stringify(object)
+            })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                } else {
+                }).catch(() => {
                     showThanksModal(message.failure);
-                }
-            });
+                }).finally(() => {
+                    form.reset();
+                });
         });
     }
 
@@ -293,6 +289,7 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000)
     }
+
 });
 
 
